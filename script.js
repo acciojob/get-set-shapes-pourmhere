2ceccd0cb55b468aad56823bec76558609e420d3
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-08-14 00:36:39 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
 
-class Square extends Animal {}
+    getWidth() {
+        return this.width;
+    }
+
+    getHeight() {
+        return this.height;
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
+        this.side = side;
+    }
+
+    getPerimeter() {
+        return 4 * this.side;
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
